0a4b261f2df157fca40732250974860190967454
MobbareKurtZ <hannes.skoog@gmail.com>
2018-02-09 14:08:21 +0100

    tja
    hej

---
+++ js/script.js
@@ -0,0 +1,35 @@
+(function($) {
+    $.fn.clickToggle = function(func1, func2) {
+        var funcs = [func1, func2];
+        this.data('toggleclicked', 0);
+        this.click(function() {
+            var data = $(this).data();
+            var tc = data.toggleclicked;
+            $.proxy(funcs[tc], this)();
+            data.toggleclicked = (tc + 1) % 2;
+        });
+        return this;
+    };
+}(jQuery));
+
+$(document).ready(function() {
+    $('.navLogin').css({'display':'none'});
+    $('.menuic').clickToggle(function() {
+            $('#navbar').animate({
+                top: "0px"
+            }, 400);
+            $('.menuic').toggleClass('rotate');
+            $('.bluroverlay').toggleClass('bluroverlayActive');
+        },
+        function() {
+            $('#navbar').animate({
+                top: "-270px"
+            }, 400);
+            $('.menuic').toggleClass('rotate');
+            $('.bluroverlay').removeClass('bluroverlayActive');
+        });
+        $('.navbtn1').clickToggle(function() {
+            $('.navLogin').css({'display':'grid'});
+            $('.navbtns').css({'display':'none'});
+        });
+});

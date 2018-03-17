69775eb4f2f6c05df063724620f760b81b152d51
BuildTools <unconfigured@null.spigotmc.org>
2018-03-17 20:26:37 +0100

    rastafari

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@
 }(jQuery));
 
 $(document).ready(function () {
-    //$('.navLogin').css({ 'display': 'none' });
     //  LOGIN ANIMATE DESKTOP
     $('.d_accic').clickToggle(function () {
         $('.navbtns').animate({
@@ -28,6 +27,10 @@ $(document).ready(function () {
                 top: "-70px"
             }, 400);
             $('#navbar > a').fadeIn(300);
+            if ($('.navLogin').css('display') == 'flex') {
+                $('.navLogin').fadeOut(400);
+                $('.navbtns').fadeIn(400);
+            };
         });
     //  NAVBAR
     $('.menuic').clickToggle(function () {
@@ -43,6 +46,12 @@ $(document).ready(function () {
             }, 400);
             $('.menuic').toggleClass('rotate');
             $('.bluroverlay').toggleClass('bluroverlayActive');
+            setTimeout(function () {
+                if ($('.navLogin').css('display') == 'flex') {
+                    $('.navLogin').css({ 'display': 'none' });
+                    $('.navbtns').css({ 'display': 'grid' });
+                };
+            }, 400);
         });
     //  LOGIN FORM
     $('.navbtn1').click(function () {
@@ -50,14 +59,10 @@ $(document).ready(function () {
         $('.navbtns').css({ 'display': 'none' });
     });
     //  SAVE
-    $('.save i').clickToggle(function () {
-        $('.savepop').css({ 'display': 'block' });
+    $('.save i').click(function () {
+        $('.savepop').toggle(400);
         $('.save').toggleClass('inactive');
-    },
-        function () {
-            $('.savepop').css({ 'display': 'none' });
-            $('.save').toggleClass('inactive');
-        });
+    });
     //  TRIPPOP LIST MOBILE
     $('.trips').clickToggle(function () {
         $('.trippop').animate({
@@ -73,33 +78,20 @@ $(document).ready(function () {
         });
     //  DELETE SWITCH
     $('.delete').clickToggle(function () {
-        $(".start i").fadeOut(300, function () {
-            $(this).text("delete").fadeIn(300);
-        });
-        $(".check i").fadeOut(300, function () {
-            $(this).text("delete").fadeIn(300);
-        });
-        $(".fin i").fadeOut(300, function () {
+        $('div[class="rlist"] .list_item i').fadeOut(300, function () {
             $(this).text("delete").fadeIn(300);
         });
     },
         function () {
-            $(".start i").fadeOut(300, function () {
-                $(this).text("near_me").fadeIn(300);
-            });
-            $(".check i").fadeOut(300, function () {
-                $(this).text("place").fadeIn(300);
-            });
-            $(".fin i").fadeOut(300, function () {
-                $(this).text("flag").fadeIn(300);
+            $('div[class="rlist"] .list_item i').fadeOut(300, function () {
+                $('div[class="rlist"] .list_item i').text('place').fadeIn(400);
+                $('div[class="rlist"] .list_item i').eq(0).text('near_me');
+                $('div[class="rlist"] .list_item i').eq($('.rlist').index() - 1).text('flag');
             });
         });
-    $('.delete i').clickToggle(function () {
+    $('.delete i').click(function () {
         $('.delete').toggleClass('inactive');
-    },
-        function () {
-            $('.delete').toggleClass('inactive');
-        });
+    });
     $(".list_item").click(function (e) {
         e.stopPropagation();
         if ($(this).children('i').text() == "delete") {
@@ -107,25 +99,41 @@ $(document).ready(function () {
         }
     });
     //  MAP TOGGLE
-    $('.map').clickToggle(function () {
-        $('.karta').css({ 'display': 'block' });
+    $('.map').click(function () {
+        $('.karta').toggle(400);
         $('.map').toggleClass('inactive');
-    },
-        function () {
-            $('.karta').css({ 'display': 'none' });
-            $('.map').toggleClass('inactive');
-        });
-    //  DEST SEARCH
-    $('.srcbar').click(function () {
-        $('.rlist .list_item i').text('add')
     });
+    //  DEST SEARCH
+    $('.srcbar').focus(function () {
+        $('div[class="rlist search"]').show();
+        $('div[class="rlist"]').hide();
+        $('.srcbar, .search').blur(function () {
+            $('.search .list_item i').click(function () {
+                var list = $(this).parent().parent().parent().clone();
+                $(list).children('.front').children().children('i').text('flag');
+                $('div[class="rlist"]').children().last().before(list);
+                $('div[class="rlist search"]').hide();
+                $('div[class="rlist"]').show();
+            });
+        });
+        $('div[class="rlist"] .list_item i').text('place');
+        $('div[class="rlist"] .list_item i').eq(0).text('near_me');
+    });
+    $(".srcbar").on("keyup", function () {
+        var g = $(this).val().toLowerCase();
+        $(".rlist div h2").each(function () {
+            var s = $(this).text().toLowerCase();
+            $(this).closest('.rlist > div')[s.indexOf(g) !== -1 ? 'show' : 'hide']();
+        });
+    });
+
     //  NEW POP
     $('#main_dgb .list > h2 i').clickToggle(function () {
-        $('.newpop').css({ 'display': 'block' });
+        $('.newpop').toggle(400);
         $('#main_dgb .list > h2 i').css({ 'background-color': '#2c5f61' });
     },
         function () {
-            $('.newpop').css({ 'display': 'none' });
+            $('.newpop').toggle(400);
             $('#main_dgb .list > h2 i').css({ 'background-color': '#042a2b' });
         });
     $('#main_dgb .reslist > h2 i').clickToggle(function () {
@@ -141,40 +149,39 @@ $(document).ready(function () {
         $('.resor').animate({
             bottom: "0px"
         }, 400);
-        $('.resclick i').toggleClass('rotate180');
+        $('.resclick i').addClass('rotate180');
     },
         function () {
             $('.resor').animate({
                 bottom: "-260px"
             }, 400);
-            $('.resclick i').toggleClass('rotate180');
-        });
-    //  BUDGET TOGGLE
-    $('.m_budget').clickToggle(function () {
-        $('.bdg1').css({ 'display': 'block' });
-        $('.bdg2').css({ 'display': 'block' });
-        $('.bdg3').css({ 'display': 'block' });
-        $('.m_budget').toggleClass('inactive');
-    },
-        function () {
-            $('.bdg1').css({ 'display': 'none' });
-            $('.bdg2').css({ 'display': 'none' });
-            $('.bdg3').css({ 'display': 'none' });
-            $('.m_budget').toggleClass('inactive');
+            $('.resclick i').removeClass('rotate180');
         });
     //  BUDGET TOTAL
     $(".sumbtn").click(function () {
         var sum = parseInt($(this).siblings(".num1").val()) + parseInt($(this).siblings(".num2").val()) + parseInt($(this).siblings(".num3").val());
+        $(this).parent('.values').siblings('.sum').val(sum + 'kr');
+    });
+    $('.sumbtn, .max').on('click, keyup', function(){
+        var tot = 0
         var max = parseInt($('.max').val());
-        $(this).parent('.values').siblings('.sum').removeClass('sumred');
-        if (sum > max) {
-            $(this).parent('.values').siblings('.sum').toggleClass('sumred');
+        for (var i = 0; i < 3; i++) {
+            var tal = parseInt($('.sum').val()) || 0;
+            tot = tot + parseInt($('.sum').eq(i).val());
+            console.log(tot);
         };
-        $(this).parent('.values').siblings('.sum').val(sum + "kr");
+        if (tot > max) {
+            $('.max').addClass('sumred');
+        } else {
+            $('.max').removeClass('sumred');
+        };
     });
     //  SELECT PERSONS + CHAT SHOW
     $('.person').clickToggle(function () {
         $(this).toggleClass('inactive');
+        i = $('.inactive').length + 1
+        var text = "\n" + "Person" + i + ": Hej!"
+        $(this).parent('.persons').siblings('.chat').append(text);
         if ($('.dest').css('height') != '360px') {
             $(this).parent('.persons').parent('.dest').css({ 'min-height': '440px' }, 400)
         };
@@ -186,10 +193,14 @@ $(document).ready(function () {
                     $(this).parent('.persons').parent('.dest').css({ 'min-height': '175px' });
                 };
             };
+            i = $(this).parent('.persons').children('.inactive').length + 2
+            var text = "\n" + "Person" + i + ": Hej!"
+            $(this).parent('.persons').siblings('.chat').text(function () {
+                return $(this).text().replace(text, '');
+            });
         });
     //  DEST VAL 
     $('.dests h4').clickToggle(function () {
-        console.log($('.dests h4').hasClass('destgrow'));
         $(this).delay(50).addClass('destgrow');
     },
         function () {
@@ -206,9 +217,9 @@ $(document).ready(function () {
     });
     $('.chatwrite').keypress(function (e) {
         if (e.which == 13) {
-        var text = "\n" + "Jag: " + $(this).val();
-        $(this).parent('.chatform').siblings('.chat').append(text);
-        $(this).val('');
+            var text = "\n" + "Jag: " + $(this).val();
+            $(this).parent('.chatform').siblings('.chat').append(text);
+            $(this).val('');
         }
     });
 
@@ -216,17 +227,21 @@ $(document).ready(function () {
     $('.rlist1').flip({ trigger: 'manual', axis: 'x', reverse: 'true' });
     $('.rlist2').flip({ trigger: 'manual', axis: 'x', reverse: 'true' });
     $('.rlist3').flip({ trigger: 'manual', axis: 'x', reverse: 'true' });
+    $('.rlist4').flip({ trigger: 'manual', axis: 'x', reverse: 'true' });
+    $('.rlist5').flip({ trigger: 'manual', axis: 'x', reverse: 'true' });
     $('.mxbdg').flip({ trigger: 'manual', axis: 'x', reverse: 'true' });
     $('.d_budget, .m_budget').click(function () {
         $('.rlist1').flip('toggle');
         $('.rlist2').flip('toggle');
         $('.rlist3').flip('toggle');
+        $('.rlist4').flip('toggle');
+        $('.rlist5').flip('toggle');
         $('.mxbdg').flip('toggle');
     });
-    $('.d_budget').clickToggle(function () {
+    $('.d_budget').click(function () {
         $('.d_budget').toggleClass('inactive');
-    },
-        function () {
-            $('.d_budget').toggleClass('inactive');
-        });
+    });
+    $('.m_budget').click(function () {
+        $('.m_budget').toggleClass('inactive');
+    });
 });
